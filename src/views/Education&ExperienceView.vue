3ef61f39c7d22a01613148c7b09a6cb2baa49a32
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-09-16 14:27:24 +0200

    Education And Experience

---
+++ src/views/Education&ExperienceView.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="container" resume-page>
+        <div class="row">
+            <h2 class="display-2">Education & Experiences</h2>
+        </div>
+        <div class="row gap-2 justify-content-center" v-if="education?.length" data-aos="fade-up" data-aos-duration="2000">
+            <Card v-for="education in education" :key="education.id" cards>
+                <template #cardHeader>
+                    <h4 class="card-title">{{ education.school }}</h4>
+                </template>
+                <template #cardBody>
+                    <img :src="education.img_url" :alt="education.school" loading="lazy" class="img-fluid">
+                    <p class="lead">{{ education.description }}</p>
+                </template>
+            </Card>
+        </div>
+        <div class="row gap-2 justify-content-center" v-if="experiences?.length" data-aos="fade-up"
+        data-aos-duration="2000">
+            <Card v-for="experiences in experiences" :key="experiences.id" cards>
+                <template #cardHeader>
+                    <h4 class="card-title">{{ experiences.company }}</h4>
+                </template>
+                <template #cardBody>
+                    <img :src="experiences.img_url" :alt="experiences.company" loading="lazy" class="img-fluid">
+                    <h5 class="lead">{{ experiences.title }}</h5>
+                    <p class="lead">{{ experiences.description }}</p>
+                </template>
+            </Card>
+        </div>
+        <Spinner v-else />
+</div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+import Spinner from '@/components/Spinner.vue';
+export default {
+    components: {
+        Card,
+        Spinner
+    },
+    computed: {
+        education() {
+            return this.$store.state.education
+        },
+        experiences() {
+            return this.$store.state.experiences
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchEducation')
+        this.$store.dispatch('fetchExperiences')
+    }
+}
+</script>
+
+<style scoped>
+/* [resume] {
+    display: flex;
+} */
+
+[cards] {
+    box-shadow: 0 0.5rem 1rem rosybrown;
+    background-color: rgb(208, 188, 177);
+    width: 40%;
+}
+.img-fluid {
+    aspect-ratio: 1;
+    object-fit: contain;
+    object-position: center;
+    height: 9rem;
+}
+</style>
