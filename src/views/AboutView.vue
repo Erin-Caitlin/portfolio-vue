d9cb198465a7732953d4d1bba110de52780792eb
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-09-16 14:11:34 +0200

    About Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container" id="aboutSection">
+    <h2 class="display-2 text-white" data-aos="zoom-in-down">About</h2>
+    <div id="info">
+        <div v-if="about" about>             
+            <p class="lead">{{ about[0].information[0] }}</p> 
+            <p class="lead">{{ about[0].information[1] }}</p> 
+            <p class="lead">{{ about[0].information[2] }}</p> 
+            <p class="lead">{{ about[0].information[3] }}</p>
+        </div>
+    <Spinner v-else/>
+    </div>
   </div>
 </template>
+
+<script setup>
+import Spinner from '@/components/Spinner.vue';
+import {computed, onMounted } from 'vue'
+import {useStore} from 'vuex'
+
+const store = useStore()
+    const about = 
+    computed(() => store.state.about)
+
+
+    onMounted(() => {
+        store.dispatch('fetchAbout')
+    })
+</script>
+<style scoped>
+#info {
+    margin: 2rem auto 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    gap: .4rem;
+    width: 70%;
+}
+
+</style>
