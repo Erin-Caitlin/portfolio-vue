b83aaf197b3de06dbc54a2badc2232a928d3f2da
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-09-16 12:42:32 +0200

    Landing Page

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
+    name: 'landing',
+    component: () => import('../views/LandingView.vue')
   },
   {
     path: '/about',
---
+++ src/views/LandingView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container-fluid p-0" landing>
+    <div class="row vh-100 align-items-center animate__animated animate__fadeInTopLeft" page>
+      <div class="col">
+        <div>
+          <img src="https://erin-caitlin.github.io/portfolio-images/VuePortfolio/image24.jpg" alt="profile" class="img-fluid w-75 shadow rounded-top" loading="lazy"> 
+        </div>
+      </div>
+      <div class="col">
+        <div class="animate__animated animate__fadeInTopLeft" id="details">
+          <h2 class="display-2">Erin Caitlin Williams</h2>
+          <h4 class="display-4">Aspiring Developer</h4>
+        </div>
+        <div id="links">
+          <router-link to="/about" class="m-2">
+            <button class="btn pro-link">ABOUT ME</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+[page] {
+  display: flex;
+  justify-content: center;
+  width: 95%;
+}
+
+.m-2 {
+  text-decoration: none;
+  color: white;
+}
+
+.btn {
+  background-color: rgba(245, 245, 220, 0.521);
+}
+
+#links {
+  margin: 1rem;
+}
+h1, h2, h3,h4, h6{
+  font-family: "Italiana", sans-serif;
+  font-weight: 400;
+  filter: drop-shadow(0.1rem 0.1rem 0.1rem black);
+  text-decoration-color: #6642428a;
+}
+</style>
